cc8d789407452e24d4cb16c60790d813b54a6d3d
sebastianBaquero98 <s.baqueroa@uniandes.edu.co>
2021-07-07 19:43:27 -0500

    [Feature]: Random frase

--- src/frase/views.py
+++ src/frase/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .models import Frase
+import random
 # Create your views here.
+def frase_view(request):
+    randNum = random.randrange(1,9)
+    obj = Frase.objects.get(id=randNum)
+
+    context = {
+        'object': obj
+    }
+    
+    return render(request, "frase.html", context)
--- src/parlApp/urls.py
+++ src/parlApp/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from pages.views import frase_view
+from frase.views import frase_view
 from django.conf import settings
 from django.conf.urls.static import static
 
