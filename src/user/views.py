03c7729a0e6c0ebf423519640a4934e35a933c57
sebastianBaquero98 <s.baqueroa@uniandes.edu.co>
2021-07-21 18:42:31 -0500

    [Feature]: Guarda usuario en la base de datos

--- src/user/views.py
+++ src/user/views.py
@@ -1,14 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import UserForm
+from .models import User
 
 # Create your views here.
 def acceso_view(request):
-    form = UserForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-    context = {
-        'form': form
-    }
-    return render(request, "acceso.html", context)
+    request.get_host()
+    if request.method == 'POST':
+        nombre = request.POST['nombre']
+        username = request.POST['username']
+        if User.objects.filter(username=username).exists():
+            print("[EXCEPTION]: El username ya existe, no se creo en DB")
+        else:
+            user = User.objects.create(nombre=nombre, username=username)
+            user.save()
+            print(f'Se creo el usuario {username} en la base de datos')
+            
+        return redirect('http://127.0.0.1:8000/frases/')
+            
+    else:
+        return render(request, "acceso.html", {})
 
  
